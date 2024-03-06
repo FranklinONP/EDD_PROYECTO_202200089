61d2b22c3f54c66b7cf6e678f3a71e837a7ce988
Franklin ONP <3110022770701@ingenieria.usac.edu.gt>
2024-03-05 19:10:21 -0600

    aaa
    aaaaa

---
+++ Fase2_EDD/app/main.f90
@@ -0,0 +1,6 @@
+program main
+  use Fase2_EDD, only: say_hello
+  implicit none
+
+  call say_hello()
+end program main
---
+++ Fase2_EDD/src/Fase2_EDD.f90
@@ -0,0 +1,10 @@
+module Fase2_EDD
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, Fase2_EDD!"
+  end subroutine say_hello
+end module Fase2_EDD
